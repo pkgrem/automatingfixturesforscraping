055f576c2189e9a9573deb4cedd18c227b4f6fa3
Paddy <paddy.goode2@mail.dcu.ie>
2021-08-31 23:45:04 +0100

    Add files via upload
    Connection to the DB via Python to get a listing of the URLs.

---
+++ Python Code/Anytime.py
@@ -0,0 +1,21 @@
+import pyodbc
+
+def read(conn):
+    print("Read")
+    cursor = conn.cursor()
+    cursor.execute("select anytimeurl from url")
+    for row in cursor:
+        print(f'row = {row}')
+    print()
+
+conn = pyodbc.connect(
+
+        "Driver= {SQL Server Native Client 11.0};"
+        "Server=localhost\FPL;"
+        "Database=master;"
+        "Trusted_Connection=yes"
+)
+
+read(conn)
+
+conn.close()
